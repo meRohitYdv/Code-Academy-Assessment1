7d96d22d010a915e78790aee251b0c91ea348b21
Rohit Yadav <me.rohit.ydv@gmail.com>
2023-01-18 12:52:49 +0530

    update: bowling utilities

--- bowlingUtility.js
+++ bowlingUtility.js
@@ -4,26 +4,32 @@ function score(throws){
     
     let frameCount=0;
     let frameThrow=0;
-    let prevThrow=0;
-    let output=0;
+    let prevThrowScore=0;
+    let result=0;
 
     for(let i=0; i<throws.length; i++){
         if(frameThrow==0 && throws[i]==10){
             result += 10+throws[i+1]+throws[i+2];
             frameCount++;
+            prevThrowScore = 0;
         }
-        if(frameThrow==1 && throws[i]==10-prevThrow){
-            result += prevThrow+throws[i]+throws[i+1];
+        if(frameThrow==1 && throws[i]==10-prevThrowScore){
+            result += prevThrowScore+throws[i]+throws[i+1];
             frameThrow = 0;
+            frameCount++;
         }
         if(frameThrow==1){
-            result += prevScore+throws[i];
+            result += prevThrowScore+throws[i];
             frameThrow = 0;
+            frameCount++;
         }
         frameThrow++;
+        prevThrowScore = throws[i];
     }
     return result;
 }
+
+console.log(score([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
 
 function bestScore(){
 
